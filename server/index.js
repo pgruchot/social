1f4de3295eead01b79ba292dc50eefcd26a50bb7
Patryk Gruchot <patryk.gruchot@outlook.com>
2023-05-02 18:40:24 +0200

    created user model, registration/login controller and routes, jwt verification middleware

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import multer from "multer";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +35,12 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;

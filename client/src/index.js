93f0f5f4708326ffb7df9fb4771a9c544c762568
Patryk Gruchot <patryk.gruchot@outlook.com>
2023-05-04 18:24:33 +0200

    app theme settings and imports/theme for navbar component

--- client/src/index.js
+++ client/src/index.js
@@ -15,20 +15,19 @@ import {
   PURGE,
   REGISTER,
 } from "redux-persist";
-import { storage } from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => {
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    });
-  },
+    }),
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
